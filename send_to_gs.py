28647b6b43c8ab9f39d0d4abf0ac4a1eb4b832af
everleazy <39529705+everleazy@users.noreply.github.com>
2023-04-06 14:39:13 +0300

    Update send_to_gs.py

--- send_to_gs.py
+++ send_to_gs.py
@@ -1,8 +1,13 @@
 # отправляет данные в подключенную GS
-def send_to_gs(sh_name, df):
+def send_to_gs(sh_name, df, json_path, sheet_id):
     import gspread  # для работы с GS
     from gspread_dataframe import set_with_dataframe  # функция отправки таблицы в GS
     from google.oauth2.credentials import Credentials  # для авторизации в Google
+    
+    #-# авторизация и подключение к листу (создание его, при отсутствии)
+    gc = gspread.oauth(credentials_filename=json_path)  # местоположение ключа oauth2
+    sh = gc.open_by_key(sheet_id)  # id таблицы
+
     # sh_name = название листа, на который будут записаны данные 
     # df = данные, которые нужно отправить
     # если указанного листа нет, то он будет создан
