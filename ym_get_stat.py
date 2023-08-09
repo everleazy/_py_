125131bf0a4be4b65e6f258193c842862711c5df
everleazy <39529705+everleazy@users.noreply.github.com>
2023-08-09 13:16:26 +0300

    Update ym_get_stat.py

--- ym_get_stat.py
+++ ym_get_stat.py
@@ -177,3 +177,65 @@ def get_ym_stat_v2(counter, limit, _date1, _date2, _metrics, _dimensions, _sort,
     #res_df.columns = [i.split(':')[2] for i in res_df.columns]
     
     return res_df
+
+
+def YandexMetrika_get_data(counter, limit, _date1, _date2, _metrics, _dimensions, _sort, client_connect, 
+                    filters, 
+                    lang='en'):
+    import math
+    import pandas as pd
+    
+    client = client_connect
+    params = dict(
+        ids=counter,
+        date1=_date1,
+        date2=_date2,  # dt.date.today()
+        metrics=_metrics,
+        dimensions=_dimensions,
+        sort=_sort,
+        lang=lang,
+        limit=limit,
+        filters=filters,
+                        
+        accuracy='full'
+        # другие параметры -> https://yandex.ru/dev/metrika/doc/api2/api_v1/data.html
+        )
+    res_df = client.stats().get(params=params)
+
+    count_iter = math.ceil(res_df['total_rows'] / limit)
+
+    print('Необходимое кол-во итераций:', count_iter)
+    if count_iter == 1:
+        res_df = pd.DataFrame(res_df().to_dicts())
+        print('Данные полностью скачены.')
+    else:
+        k = 0
+        rep = res_df
+        res_df = pd.DataFrame()
+    
+        for i in range(1, math.ceil(rep['total_rows'] / limit)+2):
+            params = dict(
+                ids=counter,
+                date1=_date1,
+                date2=_date2,  # dt.date.today()
+                metrics=_metrics,
+                dimensions=_dimensions,
+                sort=_sort,
+                lang=lang,
+                limit=limit,
+                filters=filters,
+
+                accuracy='full',
+                offset=1+k)
+    
+            print(f'....качаю с строки #{k}, итерация #{i}')
+            k += limit
+        
+            report = client.stats().get(params=params)
+    
+            res_df = pd.concat([res_df, pd.DataFrame(report().to_dicts())])
+    #clear_output()
+    print('Данные полностью скачены!')
+    print('Строк:', res_df.shape[0], '\nСтолбцов:', res_df.shape[1])
+
+    return res_df
